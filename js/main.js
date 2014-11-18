52f969472ad22f2dcc2fa835f81a48e7be394ac4
LeRiton <leriton@leriton.net>
2014-11-18 18:51:57 +0100

    Target blank is valid HTML5, remove unecessary JS

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).on('ready', function() {
-    
-    $('.external-link').on('click', function() {
-        var url = $(this).attr('href');
-        window.open(url, '_blank');
-        return false;
-    });
-});
